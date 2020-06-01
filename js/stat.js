ce3360bdebbfdbeb47399af49ae4497db76e382c
Victoria Domovets <v.domovets@gmail.com>
2020-06-01 20:50:28 +0200

    условие цвет замена на тернарный оператор

--- js/stat.js
+++ js/stat.js
@@ -35,21 +35,19 @@ window.renderStatistics = function (ctx, players, times) {
 
   ctx.font = '16px PT Mono';
   ctx.fillStyle = '#000';
-  ctx.fillText('Ура вы победили!', CLOUD_X + CLOUD_GAP * 3, CLOUD_Y + CLOUD_GAP * 3);
-  ctx.fillText('Список результатов:', CLOUD_X + CLOUD_GAP * 3, CLOUD_Y + CLOUD_GAP * 5);
+  ctx.fillText('Ура вы победили!', CLOUD_X + GAP, CLOUD_Y + GAP);
+  ctx.fillText('Список результатов:', CLOUD_X + GAP, CLOUD_Y + CLOUD_GAP * 5);
 
   for (var i = 0; i < players.length; i++) {
     ctx.fillStyle = '#000';
-    var GapX = CLOUD_X + GAP + (GAP * 2 + BAR_WIDTH) * i;
-    ctx.fillText(players[i], GapX, CLOUD_HEIGHT - CLOUD_GAP);
-    if (players[i] === 'Вы') {
-      ctx.fillStyle = 'rgba(255, 0, 0, 1)';
-    } else {
-      ctx.fillStyle = 'hsl(240, ' + Math.random() * 100 + '%, 50%)';
-    }
+    var gapX = CLOUD_X + GAP + (GAP * 2 + BAR_WIDTH) * i;
+    ctx.fillText(players[i], gapX, CLOUD_HEIGHT - CLOUD_GAP);
+
+    ctx.fillStyle = players[i] === 'Вы' ? 'rgba(255, 0, 0, 1)' : 'hsl(240, ' + Math.random() * 100 + '%, 50%)';
+
     var playerBarHeight = MAX_HEIGHT * times[i] / maxTime;
-    ctx.fillRect(GapX, GAP * 3 + MAX_HEIGHT - playerBarHeight, BAR_WIDTH, playerBarHeight);
+    ctx.fillRect(gapX, GAP * 3 + MAX_HEIGHT - playerBarHeight, BAR_WIDTH, playerBarHeight);
     ctx.fillStyle = '#000';
-    ctx.fillText(Math.round(times[i]), GapX, GAP * 2.8 + MAX_HEIGHT - playerBarHeight);
+    ctx.fillText(Math.round(times[i]), gapX, GAP * 2.8 + MAX_HEIGHT - playerBarHeight);
   }
 };
